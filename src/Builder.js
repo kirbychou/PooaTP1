194306a2a939b27b650e3f8ca9e7856219566059
Your Name <lucy.carpe@gmail.com>
2015-09-10 16:07:54 +0200

    deuxime test

---
+++ src/Builder.js
@@ -0,0 +1,29 @@
+var Contact = Contact || {};
+Contact = (function (self) {
+    "use strict";
+
+    self.Builder = function () {
+        var contact;
+        this.createMinimalContact = function (value, value1, value2) {
+            contact = new Contact.Contact(value, value1, value2);
+            return contact;
+        };
+
+        this.createContactWithProfessionalMail = function (value, value1, value2, value3) {
+            contact = new Contact.Contact(value, value1, value2);
+            contact.add_mail(new Contact.Mail(value3, Contact.MailCategory.PRO));
+            return contact;
+        };
+
+        this.createContactWithProfessionalMobile = function (value, value1, value2, value3) {
+            contact = new Contact.Contact(value, value1, value2);
+            contact.add_phone(new Contact.Phone(value3, Contact.PhoneCategory.PRO, Contact.PhoneType.MOBILE));
+            return contact;
+        };
+
+    };
+    return self;
+}(Contact || {}));
+/**
+ * Created by lcarpent on 10/09/15.
+ */
--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,7 @@ Contact = (function (self) {
     self.Gender = {MR: 1, MS: 2};
     self.MailCategory = {PERSO: 1, PRO: 2};
     self.Contact = function (value, value1, value2) {
-        var nom, prenom, genre;
+        var nom, prenom, genre, id, mail, phone;
         this.gender = function () {
             return genre;
         };
@@ -15,10 +15,28 @@ Contact = (function (self) {
         this.lastName = function () {
             return nom;
         };
+        this.mails = function () {
+            return mail;
+        };
+        this.phones = function () {
+            return phone;
+        }
+        this.add_mail = function (v) {
+            mail.push(v);
+        };
+        this.add_phone = function (tel) {
+            phone.push(tel);
+        };
         var init = function (value, value1, value2) {
             nom = value2;
             prenom = value1;
             genre = value;
+            mail = new Array(0);
+            phone = new Array(0);
+            id = 'xxxx - xxxx - xxxx - xxxx - xxxx'.replace(/ x /g, function (c) {
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
         };
 
         init(value, value1, value2);
